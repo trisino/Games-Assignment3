107e2df83762fcf05a74f8c5a7f51cf08de0dfad
Americo <104172345+ameDev21@users.noreply.github.com>
2023-02-06 23:37:41 +0100

    Add files via upload

---
+++ bin/MapConfig/cloudsmall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="cloudsmall" tilewidth="117" tileheight="69" tilecount="1" columns="1">
+ <image source="../images/mario/cloudsmall.png" width="117" height="69"/>
+</tileset>
